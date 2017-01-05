ef7d8a37ea9f68a5b5c9a468e8bdf5aa49388a0b
Zorzin <zorzin@iMac-Zorzin.local>
2017-01-05 13:06:04 +0100

    Initial Commit

---
+++ Galendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Galendar
+//
+//  Created by Zorzin on 05.01.2017.
+//  Copyright © 2017 Zorzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Galendar/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Galendar
+//
+//  Created by Zorzin on 05.01.2017.
+//  Copyright © 2017 Zorzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
